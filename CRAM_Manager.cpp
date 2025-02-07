0263f69dd94fb442973fd7727b6e7dafef337bc4
kguerns <kguerns@clemson.edu>
2025-02-07 17:39:46 -0500

    creating CRAM Manager class

---
+++ CRAM_Manager.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+
+class CRAM_Manager {
+public:
+    CRAM_Manager() {}
+
+    // Constructor
+    CRAM_Manager(std::string errBoundMode="PW_REL", 
+                 std::string compressorType="sz3", 
+                 double errBound=1e-5, 
+                 std::string losslessCompressor="zstd") 
+        : errBoundMode(errBoundMode), compressorType(compressorType),
+        errBound(errBound), losslessCompressor(losslessCompressor) {}
+
+    // Destructor
+    ~CRAM_Manager() {}
+
+    // Register Variable: registers array to be compressed, decompressed in memory map if it does not already exist and updates number of registrations
+    void registerVar(std::string varName, std::vector<size_t> shape,
+                     std::string dtype="float64", int numVectors = 1) {
+        
+    }
+    
+    // Remove
+    void remove(std::string name, int index) {}
+    
+    // Compress
+    void compress() {}
+    
+    // Decompress 
+    void decompress() {}
+    
+private:
+    std::string errBoundMode;
+    std::string compressorType;
+    double errBound;
+    std::string losslessCompressor;
+};
+
+int main() {
+    CRAM_Manager memory("PW_ABS", "sz3", 1e-5, "zstd");
+    
+}
